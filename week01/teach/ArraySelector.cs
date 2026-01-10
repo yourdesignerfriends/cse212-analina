afa97b375bb965b6308fe0cb4822090314e0c6c5
Analina Nielsen <analinanielsen@outlook.com>
2026-01-10 10:24:22 -0800

    Completed Problem 2: Array Selector Solving a Complicated Problem

--- week01/teach/ArraySelector.cs
+++ week01/teach/ArraySelector.cs
@@ -11,6 +11,23 @@ public static class ArraySelector
 
     private static int[] ListSelector(int[] list1, int[] list2, int[] select)
     {
-        return [];
+        var result = new int[select.Length];
+        var l1Idx = 0;
+        var l2Idx = 0;
+
+        for (var i = 0; i < select.Length; i++) 
+        { 
+            if (select[i] == 1) 
+            { 
+                result[i] = list1[l1Idx]; l1Idx++; 
+                } 
+            else 
+            { 
+                result[i] = list2[l2Idx]; 
+                l2Idx++; 
+            }
+        }
+
+        return result;
     }
 }
