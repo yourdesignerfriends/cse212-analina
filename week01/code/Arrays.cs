7ae9666699cdff6daa67578911dcd124a2105d04
Analina Nielsen <analinanielsen@outlook.com>
2026-01-10 22:32:15 -0800

    Completed Part 2 Using a List

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -42,5 +42,26 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        //Plan:
+        // First, get the total number of elements in the list. Then calculate the split index as count - amount,
+        // which marks where the rotation begins. Next, take the last 'amount elements as the right part and the
+        // remaining elements as the left part using GetRange. Clear the original list, then add the right part
+        // followed by the left part to rebuild the list in its rotated order.
+
+        // Visual idea:
+        // Before: [1 2 3 4 5 6 7 8 9]
+        // Rotate 3 [7 8 9 | 1 2 3 4 5 6]
+
+        int count = data.Count;
+        int splitIndex = count - amount;
+
+        List<int> rightPart = data.GetRange(splitIndex, amount);
+        List<int> leftPart = data.GetRange(0, splitIndex);
+
+        data.Clear();
+        data.AddRange(rightPart);
+        data.AddRange(leftPart);
+
     }
 }
