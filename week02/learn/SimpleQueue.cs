dac51f7dc9ed8c4f5ed905aad4ba792859033373
Analina Nielsen <analinanielsen@outlook.com>
2026-01-14 16:44:58 -0800

    Learning Activity 4: Finding Defects using Tests Completegit add .

--- week02/learn/SimpleQueue.cs
+++ week02/learn/SimpleQueue.cs
@@ -11,6 +11,10 @@
         var value = queue.Dequeue();
         Console.WriteLine(value);
         // Defect(s) Found:
+        // Enqueue inserted the value at the front of the list 
+        // (index 0) using _queue.Insert(0, value), which reversed 
+        // the order and broke the FIFO rule. Dequeue also attempted 
+        // to remove the value at index 1 instead of index 0.
 
         Console.WriteLine("------------");
 
@@ -29,6 +33,11 @@
         value = queue.Dequeue();
         Console.WriteLine(value);
         // Defect(s) Found: 
+        // In the original version, Enqueue inserted each new value at the 
+        // beginning of the list (index 0), reversing the queue order. 
+        // Dequeue also attempted to remove the value at index 1 instead of 0. 
+        // Due to these two flaws, the values ​​were not processed in FIFO order, 
+        // causing the test to fail.
 
         Console.WriteLine("------------");
 
@@ -44,7 +53,7 @@
         catch (IndexOutOfRangeException) {
             Console.WriteLine("I got the exception as expected.");
         }
-        // Defect(s) Found: 
+        // Defect(s) Found: This one is fine
     }
 
     private readonly List<int> _queue = new();
@@ -54,7 +63,7 @@
     /// </summary>
     /// <param name="value">Integer value to add to the queue</param>
     private void Enqueue(int value) {
-        _queue.Insert(0, value);
+        _queue.Add(value);// See the explanation on line 35
     }
 
     /// <summary>
@@ -66,8 +75,8 @@
         if (_queue.Count <= 0)
             throw new IndexOutOfRangeException();
 
-        var value = _queue[1];
-        _queue.RemoveAt(1);
+        var value = _queue[0]; // See the explanation on line 13
+        _queue.RemoveAt(0); 
         return value;
     }
 }
