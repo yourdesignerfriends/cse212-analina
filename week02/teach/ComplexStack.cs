13018ee31b3aa16720fb4a6fb123dffe8c28ac0d
Analina Nielsen <analinanielsen@outlook.com>
2026-01-14 17:57:25 -0800

    step 4 team activity Review a Complex Stack Example

--- week02/teach/ComplexStack.cs
+++ week02/teach/ComplexStack.cs
@@ -1,24 +1,35 @@
 ﻿public static class ComplexStack {
+
+    //This method checks if a string has balanced parentheses, brackets, 
+    // and braces and returns true only if each opening symbol has a matching 
+    // closing symbol so they appear in the correct order.
     public static bool DoSomethingComplicated(string line) {
+        // Here a stack is created to store the opening symbols as we find them.
         var stack = new Stack<char>();
+        //Here, each character in the input string is examined.
         foreach (var item in line) {
+            // If the character is an opening symbol, push it onto the stack.
             if (item is '(' or '[' or '{') {
                 stack.Push(item);
             }
+            // Else, if the character is a closing parenthesis ')'. The matching opening symbol must be ')'.
             else if (item is ')') {
                 if (stack.Count == 0 || stack.Pop() != '(')
                     return false;
             }
+            // Else, if the character is a closing bracket ']'. The matching opening symbol must be '['.
             else if (item is ']') {
                 if (stack.Count == 0 || stack.Pop() != '[')
                     return false;
             }
+            // Else, If the character is a closing brace '}' The matching opening symbol must be '{'.
             else if (item is '}') {
                 if (stack.Count == 0 || stack.Pop() != '{')
                     return false;
             }
         }
-
+        // After processing the entire string, the stack should be empty. 
+        // If the opposite happens, it means that some opening symbols were never closed.
         return stack.Count == 0;
     }
 }
